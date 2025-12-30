6ce367e04b6643a4bf5d578d6fe75cf6c2ab9ac1
murtadapy <murtada.altarouti@gmail.com>
2025-12-30 23:31:02 +0300

    Added test_logger.py

---
+++ tests/test_logger.py
@@ -0,0 +1,57 @@
+import pytest
+from rich.text import Text
+
+from portion.core import Logger
+from portion.models import cli_state
+
+logger = Logger()
+
+
+def test_logger_pulse(capsys: pytest.CaptureFixture) -> None:
+    cli_state.verbose = True
+    logger.pulse("This is a pulse message.")
+    captured = capsys.readouterr()
+    assert "This is a pulse message." in captured.out
+
+
+def test_logger_info(capsys: pytest.CaptureFixture) -> None:
+    logger.info("This is an info message.")
+    captured = capsys.readouterr()
+    assert "This is an info message." in captured.out
+
+
+def test_logger_warn(capsys: pytest.CaptureFixture) -> None:
+    logger.warn("This is a warning message.")
+    captured = capsys.readouterr()
+    assert "This is a warning message." in captured.out
+
+
+def test_logger_error(capsys: pytest.CaptureFixture) -> None:
+    logger.error("This is an error message.")
+    captured = capsys.readouterr()
+    assert "Error: This is an error message." in captured.out
+
+
+def test_logger_prompt(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr('rich.prompt.Confirm.ask', lambda _: True)
+    response = logger.prompt("Do you want to continue?")
+    assert response is True
+
+    monkeypatch.setattr('rich.prompt.Confirm.ask', lambda _: False)
+    response = logger.prompt("Do you want to continue?")
+    assert response is False
+
+
+def test_logger_print(capsys: pytest.CaptureFixture) -> None:
+    text = Text("[bold cyan]This is a printed message.[/]")
+    logger.print(text)
+    captured = capsys.readouterr()
+    assert "This is a printed message." in captured.out
+
+
+def test_logger_pulse_non_verbose(capsys: pytest.CaptureFixture) -> None:
+    cli_state.verbose = False
+    logger = Logger()
+    logger.pulse("This is a pulse message.")
+    captured = capsys.readouterr()
+    assert "This is a pulse message." not in captured.out
