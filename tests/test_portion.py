386f61a85c3fabe63e1669766006a87aa7be7e29
murtadapy <murtada.altarouti@gmail.com>
2025-12-31 23:40:27 +0300

    Added test case to test_portion.py

--- tests/test_portion.py
+++ tests/test_portion.py
@@ -1,3 +1,6 @@
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 from typer.testing import CliRunner
 
 from portion.models import cli_state
@@ -6,11 +9,10 @@ from portion.portion import Portion
 runner = CliRunner()
 
 
-def test_run() -> None:
+def test_help() -> None:
     app = Portion().cli
-
-    result = runner.invoke(app, [])
-    assert result.exit_code == 2
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
 
 
 def test_app_callback():
@@ -23,3 +25,13 @@ def test_app_callback():
 
     assert cli_state.verbose is True
     assert cli_state.auto_confirm is True
+
+
+@patch("portion.portion.load_handlers")
+def test_run(mock_load_handlers):
+    p = Portion()
+    p.cli = MagicMock()
+    p.run()
+
+    mock_load_handlers.assert_called_once_with(p.cli)
+    p.cli.assert_called_once()
